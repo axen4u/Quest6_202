569b2c5a6e67a0b0290326e7b065aeb40cb78d1e
axen4u <damaraarya1303@gmail.com>
2025-11-20 17:15:20 +0700

    final commit

--- app/src/main/java/com/example/myapplication/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/myapplication/view/uicontroller/Navigasi.kt
@@ -24,3 +24,56 @@ import com.example.myapplication.view.FormSiswa
 import com.example.myapplication.view.TampilSiswa
 import com.example.myapplication.viewmodel.SiswaViewModel
 
+// Enum Navigasi
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+// Fungsi Pembantu Navigasi
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    modifier: Modifier = Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    // PERBAIKAN: Menggunakan collectAsState() tanpa argumen 'initial ='
+    val uiState by viewModel.statusUI.collectAsState()
+
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = modifier.padding(isiRuang)
+        ) {
+            // Rute Formulir
+            composable(route = Navigasi.Formulir.name) {
+                val konteks = LocalContext.current
+
+                FormSiswa(
+                    pilihanJK = Jenisk.map { id -> konteks.resources.getString(id) },
+                    onSubmitButtonClicked = { listDataSiswa ->
+                        viewModel.setSiswa(listDataSiswa)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+
+            // Rute Detail
+            composable(route = Navigasi.Detail.name) {
+                TampilSiswa(
+
+                    statusUISiswa = uiState,
+                    onBackButtonClicked = { cancelAndBackToFormulir(navController) }
+                )
+            }
+        }
+    }
+}
