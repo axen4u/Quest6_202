775c92f3f32d06cd873adcf0c1926516479726de
axen4u <damaraarya1303@gmail.com>
2025-11-20 16:53:13 +0700

    Start FormSiswa.kt structure and TopAppBar

---
+++ app/src/main/java/com/example/myapplication/view/uicontroller/formsiswa.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    }
