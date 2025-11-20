3484ab2a7a649e17a604a81a33f14eb3b666d283
axen4u <damaraarya1303@gmail.com>
2025-11-20 16:32:53 +0700

    Create Model/DataJK.kt to hold static gender data

---
+++ app/src/main/java/com/example/myapplication/Model/DataJK.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.Model
+
+import com.example.myapplication.R
+
+object DataJK {
+    // Menggunakan R.string.laki dan R.string.wanita
+    val Jenisk = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
